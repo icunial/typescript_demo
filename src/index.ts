c0d24ca757e48501550e0934f7368df5489e2165
Ignacio Cunial <igsecu@hotmail.com>
2023-09-08 16:42:31 -0300

    Tuples and tuples arrays

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,13 @@ age = 30
 let ids: number[] = [1,2,3,4,5]
 let arr: any[] = [1, true, "Hello"]
 
+// Tuple
+let person: [number, string, boolean] = [1, "Hello", true]
+
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, "Name 1"],
+    [2, "Name 2"]
+]
