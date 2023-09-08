0d186419008d5a0c2fd482c9e3088779268d3c03
Ignacio Cunial <igsecu@hotmail.com>
2023-09-08 16:39:57 -0300

    Basic Types

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
+// Basic Types
 let id = 5;
+let company = "Traversy Media";
+let isPublished = true;
+let x = "Hello";
+let age;
+age = 30;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+// Basic Types
 let id:number = 5
+let company:string = "Traversy Media"
+let isPublished: boolean = true
+let x: any = "Hello"
+let age: number 
+
+age = 30
+
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1, true, "Hello"]
+
